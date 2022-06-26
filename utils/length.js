a19236aa777699dbce841db902984d5f8e07fd2b
Lukas Brizgys <lukenis456@gmail.com>
2022-06-26 19:35:19 +0300

    refactor: apply eslint

--- utils/length.js
+++ utils/length.js
@@ -1,11 +1,11 @@
 /**
  * Function takes array as parameter and returns it's length
- * @param {Array} array 
+ * @param {Array} array
  * @returns {number} length of the array
  */
 const length = (array) => {
   let l = 0;
-  for(const element of array) l++;
+  for (const element of array) l += 1;
   return l;
-}
+};
 export default length;
--- utils/slice.js
+++ utils/slice.js
@@ -1,8 +1,9 @@
-import push from "./push";
+import push from './push';
+
 const slice = (array, start, end) => {
-    let result = [];
-    for(let i = start; i < end; i++) result = push(result, array[i]);
-    return result;
-}
+  let result = [];
+  for (let i = start; i < end; i += 1) result = push(result, array[i]);
+  return result;
+};
 
 export default slice;
